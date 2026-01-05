3d0862299a544b9faa715cced8c304655f89fe0c
ra+ <jo000917@naver.com>
2026-01-05 15:37:53 +0900

    create aws scheduler python

--- infra/terraform/lambda/scheduler.py
+++ infra/terraform/lambda/scheduler.py
@@ -5,12 +5,12 @@ def lambda_handler(event, context):
     # 테라폼 apply 이후 생성될 때 새 인스턴스 ID를 나중에 삽입
     # 아니면 태그 기반으로 찾도록 수정 가능
 
-    isinstance_id = event.get('instance_id', 'i-')
+    instance_id = 'i-0b15c0db671547af7'
     action = event.get('action')
 
     if action == 'start':
-        ec2.start_instances(InstanceIds=[isinstance_id])
-        return f"Started instance: {isinstance_id}"
+        ec2.start_instances(InstanceIds=[instance_id])
+        return f"Started instance: {instance_id}"
     elif action == 'stop':
-        ec2.stop_instances(InstanceIds=[isinstance_id])
-        return f"Stopped instance: {isinstance_id}"
+        ec2.stop_instances(InstanceIds=[instance_id])
+        return f"Stopped instance: {instance_id}"
